a7b7340c1114af1e71c8b16ea1ed8e2e27452f2b
mudssky <mudssky@gmail.com>
2021-08-17 14:48:32 +0800

    ✨ feat: adjust size

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -3,31 +3,38 @@ import Link from 'next/link'
 
 export default function Header(props: any) {
   return (
-    <div className="flex justify-between border-b-2 border-gray-50 h-12">
+    <div>
       <div
-        className="w-16 flex items-center justify-center text-2xl text-blue-500"
-        onClick={props.handleLogoClick}
+        className="m-auto flex justify-between border-b-2 border-gray-50 h-12"
+        style={{
+          maxWidth: '1200px',
+        }}
       >
-        <Link href="/">
-          <a>Blog</a>
-        </Link>
-      </div>
-      <div className="flex w-3/4 justify-end space-x-6">
-        <div className="flex items-center text-2xl font-mono  border-b-2 border-opacity-0 border-green-500 hover:border-opacity-100">
-          <Link href="/posts/test">
-            <a>test</a>
+        <div
+          className="w-16 flex items-center justify-center text-2xl text-blue-500"
+          onClick={props.handleLogoClick}
+        >
+          <Link href="/">
+            <a>Blog</a>
           </Link>
         </div>
-        <a
-          href="https://github.com/mudssky"
-          target="_blank"
-          rel="noreferrer"
-          className="flex text-lg items-center text-gray-600 font-sans font-semibold"
-        >
-          GitHub
-          {/* prettier-ignore */}
-          <span className="ml-1 mt-1"><svg className="fill-current text-gray-400"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="15" height="15"><path fill="none" d="M0 0h24v24H0z"/><path d="M10 6v2H5v11h11v-5h2v6a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h6zm11-3v8h-2V6.413l-7.793 7.794-1.414-1.414L17.585 5H13V3h8z"/></svg></span>
-        </a>
+        <div className="flex w-3/4 justify-end space-x-6">
+          <div className="flex items-center text-2xl font-mono  border-b-2 border-opacity-0 border-green-500 hover:border-opacity-100">
+            <Link href="/posts/test">
+              <a>test</a>
+            </Link>
+          </div>
+          <a
+            href="https://github.com/mudssky"
+            target="_blank"
+            rel="noreferrer"
+            className="flex text-lg items-center text-gray-600 font-sans font-semibold"
+          >
+            GitHub
+            {/* prettier-ignore */}
+            <span className="ml-1 mt-1"><svg className="fill-current text-gray-400"  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="15" height="15"><path fill="none" d="M0 0h24v24H0z"/><path d="M10 6v2H5v11h11v-5h2v6a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V7a1 1 0 0 1 1-1h6zm11-3v8h-2V6.413l-7.793 7.794-1.414-1.414L17.585 5H13V3h8z"/></svg></span>
+          </a>
+        </div>
       </div>
     </div>
   )
--- components/layout.tsx
+++ components/layout.tsx
@@ -9,21 +9,28 @@ export const siteTitle = 'Next.js Sample Website'
 export default function Layout(props: any) {
   return (
     <div>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content="Learn how to build a personal website using Next.js"
-        />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
-        ></meta>
-      </Head>
-      <Header handleLogoClick={props.handleLogoClick}></Header>
-      <main className={props.className}>{props.children}</main>
+      <div
+        style={{
+          maxWidth: '1920px',
+        }}
+        className="m-auto"
+      >
+        <Head>
+          <link rel="icon" href="/favicon.ico" />
+          <meta
+            name="description"
+            content="Learn how to build a personal website using Next.js"
+          />
+          <meta name="og:title" content={siteTitle} />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+          ></meta>
+        </Head>
+        <Header handleLogoClick={props.handleLogoClick}></Header>
+        <main className={props.className}>{props.children}</main>
+      </div>
     </div>
   )
 }
